eb5c965a26f2cb7f27323f4db2093dc946dc653b
ArthurDMousinho <artdmous@gmail.com>
2026-07-17 13:36:23 -0300

    Implement free fall distance calculation
    Code destined to solve free fall physics problems.

---
+++ queda_livre.py
@@ -0,0 +1,9 @@
+# Defining physics variables
+gravity = 9.8  # m/s²
+time = 3       # seconds
+
+# Calculating free fall distance
+distance = (gravity * (time ** 2)) / 2
+
+# Displaying the final result
+print(f"The height of the cliff is: {distance} meters.")
