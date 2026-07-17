4afcc6e943f15760831364abcfa0a8fb2b715fbd
ArthurDMousinho <artdmous@gmail.com>
2026-07-17 14:01:16 -0300

    Third python challenge
    Third attempt to programing using python in my native language.

---
+++ desafio 3 - aula 4 - success.py
@@ -0,0 +1,10 @@
+name = input('Hello! Welcome. What is your name? ')
+
+print(f'It is a pleasure, {name}.')
+
+num1 = int(input('What is the first number (no , and .)? '))
+num2 = int(input('What is the second number? (no , and .) '))
+
+result = num1 + num2
+
+print(f'The addition of {num1} and {num2} is: {result}')
